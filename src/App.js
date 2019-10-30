2bd713f4a13ca308b34ab65e1514918f8bf523d0
Agata Stawarz <agata.stawarz94@gmail.com>
2019-10-30 14:48:19 +0100

    Add React Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { HabitList } from './components/HabitList';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { route } from './config/routes';
 
 function App() {
   return (
-      <HabitList />
+    <Router>
+      <Route exact path={route.root()} component={HabitList} />
+    </Router>
   );
 }
 
